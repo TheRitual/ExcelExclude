06ba6dac6de3d48857c11b7fcfd0412e6781f9e5
TheRitual <dev@theritual.eu>
2020-03-22 18:14:29 +0100

    Finished main Layout

--- ExcelExclude/Form1.Designer.cs
+++ ExcelExclude/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExcelExclude
 {
-    partial class Form1
+    partial class fileButtonA
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,312 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fileButtonA));
+            this.exit = new System.Windows.Forms.Button();
+            this.fileLabelA = new System.Windows.Forms.Label();
+            this.fileLabelB = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileButtonB = new System.Windows.Forms.Button();
+            this.comboSheetA = new System.Windows.Forms.ComboBox();
+            this.comboSheetLabelA = new System.Windows.Forms.Label();
+            this.comboSheetLabelB = new System.Windows.Forms.Label();
+            this.comboSheetB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboColumnA = new System.Windows.Forms.ComboBox();
+            this.comboColumnB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.logLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.labelPathA = new System.Windows.Forms.Label();
+            this.labelPathB = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exit.Location = new System.Drawing.Point(672, 380);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(120, 50);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "Zamknij";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // fileLabelA
+            // 
+            this.fileLabelA.AutoSize = true;
+            this.fileLabelA.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.fileLabelA.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fileLabelA.Location = new System.Drawing.Point(12, 54);
+            this.fileLabelA.Name = "fileLabelA";
+            this.fileLabelA.Size = new System.Drawing.Size(45, 19);
+            this.fileLabelA.TabIndex = 1;
+            this.fileLabelA.Text = "Plik A";
+            // 
+            // fileLabelB
+            // 
+            this.fileLabelB.AutoSize = true;
+            this.fileLabelB.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.fileLabelB.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fileLabelB.Location = new System.Drawing.Point(204, 54);
+            this.fileLabelB.Name = "fileLabelB";
+            this.fileLabelB.Size = new System.Drawing.Size(45, 19);
+            this.fileLabelB.TabIndex = 2;
+            this.fileLabelB.Text = "Plik B";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(10, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Wybierz Plik A";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileButtonB
+            // 
+            this.fileButtonB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.fileButtonB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fileButtonB.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fileButtonB.Location = new System.Drawing.Point(203, 80);
+            this.fileButtonB.Name = "fileButtonB";
+            this.fileButtonB.Size = new System.Drawing.Size(168, 37);
+            this.fileButtonB.TabIndex = 4;
+            this.fileButtonB.Text = "Wybierz Plik B";
+            this.fileButtonB.UseVisualStyleBackColor = false;
+            // 
+            // comboSheetA
+            // 
+            this.comboSheetA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboSheetA.FormattingEnabled = true;
+            this.comboSheetA.Location = new System.Drawing.Point(10, 163);
+            this.comboSheetA.Name = "comboSheetA";
+            this.comboSheetA.Size = new System.Drawing.Size(168, 21);
+            this.comboSheetA.TabIndex = 5;
+            // 
+            // comboSheetLabelA
+            // 
+            this.comboSheetLabelA.AutoSize = true;
+            this.comboSheetLabelA.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboSheetLabelA.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboSheetLabelA.Location = new System.Drawing.Point(12, 127);
+            this.comboSheetLabelA.Name = "comboSheetLabelA";
+            this.comboSheetLabelA.Size = new System.Drawing.Size(99, 19);
+            this.comboSheetLabelA.TabIndex = 6;
+            this.comboSheetLabelA.Text = "Arkusz Pliku A";
+            // 
+            // comboSheetLabelB
+            // 
+            this.comboSheetLabelB.AutoSize = true;
+            this.comboSheetLabelB.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboSheetLabelB.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboSheetLabelB.Location = new System.Drawing.Point(204, 127);
+            this.comboSheetLabelB.Name = "comboSheetLabelB";
+            this.comboSheetLabelB.Size = new System.Drawing.Size(99, 19);
+            this.comboSheetLabelB.TabIndex = 7;
+            this.comboSheetLabelB.Text = "Arkusz Pliku B";
+            // 
+            // comboSheetB
+            // 
+            this.comboSheetB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboSheetB.FormattingEnabled = true;
+            this.comboSheetB.Location = new System.Drawing.Point(203, 163);
+            this.comboSheetB.Name = "comboSheetB";
+            this.comboSheetB.Size = new System.Drawing.Size(168, 21);
+            this.comboSheetB.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Kolumna Arkusz A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(204, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Kolumna Arkusz B";
+            // 
+            // comboColumnA
+            // 
+            this.comboColumnA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboColumnA.FormattingEnabled = true;
+            this.comboColumnA.Location = new System.Drawing.Point(10, 242);
+            this.comboColumnA.Name = "comboColumnA";
+            this.comboColumnA.Size = new System.Drawing.Size(168, 21);
+            this.comboColumnA.TabIndex = 11;
+            // 
+            // comboColumnB
+            // 
+            this.comboColumnB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboColumnB.FormattingEnabled = true;
+            this.comboColumnB.Location = new System.Drawing.Point(203, 242);
+            this.comboColumnB.Name = "comboColumnB";
+            this.comboColumnB.Size = new System.Drawing.Size(168, 21);
+            this.comboColumnB.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(10, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 33);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Excel Exclude";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(10, 317);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(640, 113);
+            this.textBox1.TabIndex = 14;
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.logLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logLabel.Location = new System.Drawing.Point(12, 278);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(32, 19);
+            this.logLabel.TabIndex = 15;
+            this.logLabel.Text = "Log";
+            this.logLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.infoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.infoLabel.Cursor = System.Windows.Forms.Cursors.Help;
+            this.infoLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.Location = new System.Drawing.Point(391, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(401, 101);
+            this.infoLabel.TabIndex = 16;
+            this.infoLabel.Text = "Wybierz Plik A";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelPathA
+            // 
+            this.labelPathA.AutoSize = true;
+            this.labelPathA.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelPathA.Location = new System.Drawing.Point(388, 127);
+            this.labelPathA.Name = "labelPathA";
+            this.labelPathA.Size = new System.Drawing.Size(52, 13);
+            this.labelPathA.TabIndex = 17;
+            this.labelPathA.Text = "PLIK A :::";
+            // 
+            // labelPathB
+            // 
+            this.labelPathB.AutoSize = true;
+            this.labelPathB.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelPathB.Location = new System.Drawing.Point(388, 163);
+            this.labelPathB.Name = "labelPathB";
+            this.labelPathB.Size = new System.Drawing.Size(52, 13);
+            this.labelPathB.TabIndex = 18;
+            this.labelPathB.Text = "PLIK B :::";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(446, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(346, 23);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "label4";
+            // 
+            // fileButtonA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(804, 442);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelPathB);
+            this.Controls.Add(this.labelPathA);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.logLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboColumnB);
+            this.Controls.Add(this.comboColumnA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboSheetB);
+            this.Controls.Add(this.comboSheetLabelB);
+            this.Controls.Add(this.comboSheetLabelA);
+            this.Controls.Add(this.comboSheetA);
+            this.Controls.Add(this.fileButtonB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fileLabelB);
+            this.Controls.Add(this.fileLabelA);
+            this.Controls.Add(this.exit);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ExcelExclude.Properties.Settings.Default, "AppName", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(820, 480);
+            this.MinimumSize = new System.Drawing.Size(820, 480);
+            this.Name = "fileButtonA";
+            this.Text = global::ExcelExclude.Properties.Settings.Default.AppName;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label fileLabelA;
+        private System.Windows.Forms.Label fileLabelB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button fileButtonB;
+        private System.Windows.Forms.ComboBox comboSheetA;
+        private System.Windows.Forms.Label comboSheetLabelA;
+        private System.Windows.Forms.Label comboSheetLabelB;
+        private System.Windows.Forms.ComboBox comboSheetB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboColumnA;
+        private System.Windows.Forms.ComboBox comboColumnB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label logLabel;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label labelPathA;
+        private System.Windows.Forms.Label labelPathB;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- ExcelExclude/Form1.cs
+++ ExcelExclude/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace ExcelExclude
 {
-    public partial class Form1 : Form
+    public partial class fileButtonA : Form
     {
-        public Form1()
+        public fileButtonA()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
